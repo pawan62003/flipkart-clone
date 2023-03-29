0b33373eee00fc210f8a8dbb75f7782633cec7e7
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2023-03-29 20:12:50 +0530

    some

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import { AdminNavbar } from '../Components/AdminNavbar'
+
+const initialState={
+    image:"",
+    title:"",
+    price:0,
+
+}
 
 export const Admin = () => {
+const [product,setProduct]=useState(initialState)
+const dispatch=useDispatch()
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log("Got submit")
+}
   return (
-    <div>Admin</div>
+    <div>
+        <h3>Welcome Admin</h3>
+        <AdminNavbar/>
+        <form onSubmit={handleSubmit}>
+            <input type="url" placeholder='Image URL' />
+          <input type="text" placeholder='Title'/>
+          <input type="text" placeholder='Price'/>
+          <input type="text" placeholder='Brand'/>
+          <input type="submit" />
+        </form>
+          
+    </div>
   )
 }
