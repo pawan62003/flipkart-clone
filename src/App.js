b3589f7a578775ee8c9d0d48102b64f652057574
BhagyeshKhalode <sonukhalode627@gmail.com>
2023-03-30 23:41:52 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { Admin } from './Pages/Admin';
+import * as React from "react";
+// import Homescreen from "./component/Homescreen";
+import { AllRoutes } from "./Pages/AllRoutes";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+// import Footer from "./Components/Footer";
+// import ErrorPage from "./Pages/ErrorPage";
+// import Dashbord from "./component/Pages/Dashbord";
+
+// import Contact from "./component/Pages/Contact";
+// import NotFound from "./component/Pages/NotFound";
+// import Login from "./component/Pages/Login";
+
+// import Products from "./component/Pages/Products";
 
 function App() {
-  return (
-    <div className="App">
-<Admin/>
-    </div>
-  );
+	// 2. Wrap ChakraProvider at the root of your app
+
+	const theme = extendTheme({
+		Link: {
+			baseStyle: {
+				textDecoration: "none",
+				_hover: {
+					textDecoration: "none",
+				},
+			},
+		},
+	});
+	return (
+		<ChakraProvider theme={theme}>
+			<AllRoutes />
+		</ChakraProvider>
+	);
 }
 
 export default App;
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import { Admin } from './Admin'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Admin } from "./Admin";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+import HomePage from "./HomePage";
+import ProductDetail from "./ProductDetail";
+import ProductPage from "./ProductPage";
+
 export const AllRoutes = () => {
-  return (
-    <Routes>
-<Route path='/admin' element={<Admin/>}/>
-    </Routes>
-  )
-}
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/cart" element={<Cart />} />
+			<Route path="/admin" element={<Admin />} />
+			<Route path="*" element={<ErrorPage />} />
+			<Route path="/product" element={<ProductPage/>} />
+			<Route path="/product/:id" element={<ProductDetail/>} />
+		</Routes>
+	);
+};
