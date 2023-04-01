21e2830faf98038974456c7223ed03bec2a24a5d
pawan kumar <pawan6200327812@gmail.com>
2023-04-01 09:41:26 +0530

    sol

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -5,14 +5,21 @@ import  {Admin}  from "./Admin";
 import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import HomePage from "./HomePage";
+import ProductDetail from "./ProductDetail";
+import ProductPage from "./ProductPage";
+import Ordercheck from "./ordercheck";
 
 export const AllRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			<Route path="/cart" element={<Cart />} />
-			<Route path="/admin" element={<Admin />} />
+			<Route path="/dashboard/admin" element={<Admin/>}/>
+			<Route path="/dashboard" element={<Dashboard />} />
 			<Route path="*" element={<ErrorPage />} />
+			<Route path="/product" element={<ProductPage/>} />
+			<Route path="/ordercheck" element={<Ordercheck />} />
+			<Route path="/product/:id" element={<ProductDetail/>} />
 		</Routes>
 	);
 };
