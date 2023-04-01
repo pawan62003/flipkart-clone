2661ee63848cc1058a46836d66a99e51bf11d11c
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2023-04-01 18:37:08 +0530

    some

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Dashboard } from "../Components/Admin/Dashboard";
-import  {Admin}  from "./Admin";
 import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import HomePage from "./HomePage";
 import ProductDetail from "./ProductDetail";
 import ProductPage from "./ProductPage";
+import { EditProducts } from "./EditProducts";
+import { Admin } from "./Admin";
 
 export const AllRoutes = () => {
 	return (
@@ -14,6 +15,7 @@ export const AllRoutes = () => {
 			<Route path="/" element={<HomePage />} />
 			<Route path="/cart" element={<Cart />} />
 			<Route path="/dashboard/admin" element={<Admin/>}/>
+			<Route path="/edit/:id" element={<EditProducts/>}/>
 			<Route path="/dashboard" element={<Dashboard />} />
 			<Route path="*" element={<ErrorPage />} />
 			<Route path="/product" element={<ProductPage/>} />
--- src/redux/ProductReducer/action.js
+++ src/redux/ProductReducer/action.js
@@ -1,16 +1,18 @@
-import { ADD_PRODUCT_SUCCESS, PRODUCT_FAILURE, PRODUCT_REQUEST } from "./actionTypes"
+import { ADD_PRODUCT_SUCCESS, PATCH_PRODUCT_SUCCESS, PRODUCT_FAILURE, PRODUCT_REQUEST } from "./actionTypes"
 import axios from "axios"
 export const addProduct=(data)=>(dispatch)=>{
     dispatch({type:PRODUCT_REQUEST})
-    try {
-    fetch('https://weak-pink-rabbit-gear.cyclic.app/Products', {
-    method: 'POST', 
-    mode: 'cors', 
-    body: JSON.stringify(data) 
+  axios.post('https://weak-pink-rabbit-gear.cyclic.app/Products',data).then(()=>{
+    dispatch({type:ADD_PRODUCT_SUCCESS})
+  }).catch(()=>{
+    dispatch({type:PRODUCT_FAILURE})
   })
-  dispatch({type:ADD_PRODUCT_SUCCESS})
-    } catch (error) {
-        dispatch({type:PRODUCT_FAILURE})
-    }
- 
 }
+export const editData=(dataObj,id)=>(dispatch)=>{
+  dispatch({type:PRODUCT_REQUEST})
+  return axios.patch(`https://weak-pink-rabbit-gear.cyclic.app/Products${id}`,dataObj).then(()=>{
+      dispatch({type:PATCH_PRODUCT_SUCCESS})
+  }).catch(()=>{
+      dispatch({type:PRODUCT_FAILURE})
+  })
+  }
