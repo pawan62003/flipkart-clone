9056aa9f1d20789d8947059eb95558c60e2e827b
0x-Sarthak <sarthakahuja447@gmail.com>
2023-04-02 23:52:55 +0530

    Updates

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-import React, { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { addProduct } from '../redux/ProductReducer/action'
-import styled from 'styled-components'
-import { Link, useNavigate } from 'react-router-dom'
-import { AdminNavbar } from '../Components/Admin/AdminNavbar'
-=======
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../redux/ProductReducer/action";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Link } from "@chakra-ui/react";
->>>>>>> Stashed changes
 
 const initialState = {
 	image: "",
@@ -22,18 +13,6 @@ const initialState = {
 	category: "",
 };
 
-<<<<<<< Updated upstream
-const thStyle={
-    paddinBbottom:" 12px",
-    textAlign:" left",
-    backgroundColor:"yellow",
-    color: "black",
-    textAlign:" center",
-}
-export const Admin = () => {
-const [product,setProduct]=useState(initialState)
-const dispatch=useDispatch()
-=======
 const thStyle = {
 	paddinBbottom: " 12px",
 	textAlign: " left",
@@ -46,7 +25,6 @@ export const Admin = () => {
 	const navigate = useNavigate();
 	const [product, setProduct] = useState(initialState);
 	const dispatch = useDispatch();
->>>>>>> Stashed changes
 
 	const handleChange = (e) => {
 		e.preventDefault();
@@ -56,48 +34,10 @@ export const Admin = () => {
 		});
 	};
 
-<<<<<<< Updated upstream
-const handleSubmit = (e) =>{
-    e.preventDefault()
-   dispatch(addProduct(product))
-   console.log(product);
-   
-   setProduct(initialState)
-}
-
-  return (
-   
-    <div style={{border:"1px solid black",marginTop:"15px",backgroundColor:"beige",height:"700px"}}>
-       <AdminNavbar/>
-        <h3 style={{marginBottom:"10px",fontSize:"25px"}}>Add New Products</h3>
-       
-        <DIV>
-             <form onSubmit={handleSubmit}>
-            <input type="text"  onChange={(e)=>{handleChange(e)}} placeholder='Image URL'name="image" value={product.image}/>
-          <input type="text"  onChange={(e)=>{handleChange(e)}} placeholder='Title' name='title' value={product.title}/>
-          <input type="text"  onChange={(e)=>{handleChange(e)}} placeholder='Price' name='price' value={product.price}/>
-          <select name="category" id="">
-            <option value="">Select Category</option>
-            <option value="t-shirts">Men</option>
-            <option value="women">Women</option>
-            <option value="kids">Kids</option>
-            <option value="spice">Spices</option>
-            <option value="vegetable">Vegetables</option>
-            <option value="dairy">Dairy Goods</option>
-            <option value="grain">Whole Grain</option>
-          </select>
-         <button type='submit'>Add</button>
-        </form>
-        </DIV>   
-    </div>
-  )
-}
-=======
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		dispatch(addProduct(product));
 		console.log(product);
->>>>>>> Stashed changes
 
 		setProduct(initialState);
 	};
@@ -202,42 +142,6 @@ const handleSubmit = (e) =>{
 	);
 };
 
-<<<<<<< Updated upstream
-input{
-  border-radius: 5px;
-    width:90%;
-    height:40px;
-    font-size: large;
-    text-align:center;
-    
-}
-button{
-  border:1px solid black;
-    width:50%;
-    height:35px;
-    border-radius: 5px;
-}
-form{
-  /* background-color: beige; */
-  background: linear-gradient(to top, #fbff88, 20%, #13535399);
-  /* border:1px solid black; */
-  width: 30%;
-    display: flex;
-    flex-direction: column;
-    gap:15px;
-align-items: center;
-justify-items: center;
-margin: auto;
-padding: 50px;
-}
-select{
-    width:50%;
-    height:30px;
-    font-size: large;
-    border-radius: 5px;
-}
-`;
-=======
 const DIV = styled.div`
 	border: 1px solid black;
 	padding: 0;
@@ -278,4 +182,3 @@ const DIV = styled.div`
 		border-radius: 5px;
 	}
 `;
->>>>>>> Stashed changes
