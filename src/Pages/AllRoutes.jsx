b77a52376f95571d086b6a4a077261e9ecd2f666
0x-Sarthak <sarthakahuja447@gmail.com>
2023-04-02 17:14:25 +0530

    hon=mepage updated

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Dashboard } from "../Components/Admin/Dashboard";
-import  {Admin}  from "./Admin";
 import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import { Admin } from "./Admin";
@@ -9,22 +8,25 @@ import HomePage from "./HomePage";
 import ProductDetail from "./ProductDetail";
 import ProductPage from "./ProductPage";
 import Ordercheck from "./ordercheck";
-import OrderHistory from './orderHistory';
-import {EditProducts} from "./EditProducts";
+import OrderHistory from "./orderHistory";
+import { EditProducts } from "./EditProducts";
+
+import LoginSignUp from "./LoginSignUp";
 
 export const AllRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			<Route path="/cart" element={<Cart />} />
-			<Route path="/dashboard/admin" element={<Admin/>}/>
-			<Route path="/edit/:id" element={<EditProducts/>}/>
+			<Route path="/dashboard/admin" element={<Admin />} />
+			<Route path="/edit/:id" element={<EditProducts />} />
 			<Route path="/dashboard" element={<Dashboard />} />
 			<Route path="*" element={<ErrorPage />} />
-			<Route path="/product" element={<ProductPage/>} />
+			<Route path="/product" element={<ProductPage />} />
 			<Route path="/ordercheck" element={<Ordercheck />} />
-			<Route path="/product/:id" element={<ProductDetail/>} />
+			<Route path="/product/:id" element={<ProductDetail />} />
 			<Route path="orderhistory" element={<OrderHistory />} />
+			<Route path="/login" element={<LoginSignUp />} />
 		</Routes>
 	);
 };
